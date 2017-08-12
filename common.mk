acf43027798f8ba531a76075a6829af67fcc4002
Elias Konstantinidis <ekondis@gmail.com>
2017-08-12 13:42:34 +0300

    Separate env var for OpenCL lib folder

--- common.mk
+++ common.mk
@@ -1,3 +1,4 @@
 # Build configuration settings
-CUDA_INSTALL_PATH = /usr/local/cuda
-OPENCL_INSTALL_PATH = /usr/local/cuda
+CUDA_INSTALL_PATH ?= /usr/local/cuda
+OPENCL_INSTALL_PATH ?= /usr/local/cuda
+OPENCL_LIBRARY_PATH ?= ${OPENCL_INSTALL_PATH}/lib
